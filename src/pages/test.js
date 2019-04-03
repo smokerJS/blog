7e0448795883067bfe5beb75f2a885199b659767
“Jung <smokerjs@stunitas.com>
2019-04-03 20:43:02 +0900

    blog-gatsby-default-setting

---
+++ src/pages/test.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "gatsby"
+import './test.scss'
+import Layout from "../components/layout"
+
+
+const TestPage = () => (
+  <Layout>
+    <div class="test">
+      빼에에에에엑
+    </div>
+    <Link to="/">Go to index</Link>
+  </Layout>
+)
+
+export default TestPage
