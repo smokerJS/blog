111c5c01a1f3490b12a2b5f38952d5a82343aaba
Jung, jinsol <smokerjs.dev@gmail.com>
2019-04-10 13:45:48 +0900

    mobile default setting

--- src/components/globalNavigationBar.js
+++ src/components/globalNavigationBar.js
@@ -2,27 +2,47 @@ import { Link } from "gatsby";
 // import PropTypes from "prop-types";
 import React from "react";
 
+class GlobalNavigationBar extends React.Component{
 
-const GlobalNavigationBar = () => (
-  <nav className="nav-global-side-menu">
+  state = {
+    open: false
+  }
 
-    <div className="profile-group">
-      <img src={require("@image/common/smokerjs-profile.jpeg")}/>
-      <dl>
-        <dt>직업</dt>
-        <dd>개발조무사</dd>
-        <dt>특기</dt>
-        <dd>고기태우기</dd>
-        <dt>목표</dt>
-        <dd>지구대폭발</dd>
-      </dl>
-    </div>
+  openHandler = () => {
+    this.setState({
+      open: !this.state.open
+    })
+  }
 
-    <ul>
+  render() {
+    return (
+      <section className={`nav-global-side-menu ${this.state.open && 'open'}`}>
+        <div id="menuOpenBtn" onClick={this.openHandler}>
+          메뉴판
+        </div>
+        <nav>
+          <div className="profile-group">
+            <img src={require("@image/common/smokerjs-profile.jpeg")}/>
+            <dl>
+              <dt>직업</dt>
+              <dd>개발조무사</dd>
+              <dt>특기</dt>
+              <dd>고기태우기</dd>
+              <dt>목표</dt>
+              <dd>지구대폭발</dd>
+            </dl>
+          </div>
 
-    </ul>
-  </nav>
-)
+          <ul>
+
+          </ul>
+        </nav>
+      </section>
+    )
+  }
+
+
+}
 
 // GlobalNavigationBar.propTypes = {
 
