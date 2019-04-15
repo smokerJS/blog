c57652ebc9faa37d7d7a206c8b6af8b7e884a984
Jung, jinsol <smokerjs.dev@gmail.com>
2019-04-15 16:03:05 +0900

    lading location setting

--- src/components/loading.js
+++ src/components/loading.js
@@ -3,7 +3,7 @@ import React from "react"
 const Loading = ({view}) => (
   <section className={`loading-background ${view ? 'view' : ''}`}>
     <div>
-      <strong>조금만 기다려 헤헤헤헤헿</strong>
+      <strong>조금만 기다려 헤헤헤</strong>
     </div>
   </section>
 )
