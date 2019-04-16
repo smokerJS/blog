9ce3fe7aeb0afceb9919cbdd30a772dbd826bbc4
Jung, jinsol <smokerjs.dev@gmail.com>
2019-04-16 14:02:44 +0900

    post data insert

--- src/template/template.js
+++ src/template/template.js
@@ -1,20 +1,18 @@
 import React from "react";
 import { graphql } from "gatsby";
-import Layout from "@components/layout";
 import SEO from "@components/seo";
 
 export default function Template({ data }) {
   const { markdownRemark: post } = data;
-
   return (
-    <Layout menu={false}>
+    <React.Fragment>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
       <div className="container">
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
         <span className="has-text-grey-light is-size-6">{post.frontmatter.date}</span>
       </div>
-    </Layout>
+    </React.Fragment>
   );
 }
 
