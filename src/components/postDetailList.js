75b8fc6af7489ec3a9fce0d41eb003787a91999f
Jung, jinsol <smokerjs.dev@gmail.com>
2019-04-22 12:56:42 +0900

    postDetailList Pagination

--- src/components/postDetailList.js
+++ src/components/postDetailList.js
@@ -5,23 +5,41 @@ import { graphql, Link } from "gatsby";
 class PostDetailList extends React.Component {
 
   state = {
-    list : this.props.list.filter(obj => obj.node.frontmatter.category === this.props.category)
+    list : this.props.list,
+    currList : this.props.list.filter(obj => obj.node.frontmatter.category === this.props.category),
+    currPage : 0,
+    prevPage : false,
+    nextPage : false
   }
 
-  page = () => {
-    let propsList = this.props.list.filter(obj => obj.node.frontmatter.category === this.props.category);
+  componentDidMount() {
+    let propsList = this.state.currList;
     let tempList = [];
     let index = 0;
     let currPage = 0;
     const list = [];
+    let pathname = window.location.pathname
+    pathname[pathname.length - 1] === '/' && (pathname = pathname.slice(0,pathname.length - 1));
     while(propsList.length) {
-      if(index % 5 === 0) {
+      propsList[0].node.frontmatter.path === pathname && (currPage = index);
+      if(index && index % 5 === 0) {
         list.push(tempList);
         tempList = [];
       }
       tempList.push(propsList.shift());
+      index++;
     }
+    tempList.length && list.push(tempList);
+    currPage = parseInt(currPage / 5);
+    this.setState({
+      list : list,
+      currList : list[currPage],
+      currPage : currPage,
+      prevPage : !currPage,
+      nextPage : !list[currPage + 1]
+    })
   }
+
 
   getCategoryName = () => {
     let result = ''
@@ -34,13 +52,23 @@ class PostDetailList extends React.Component {
     return result;
   }
 
+  onPagemoveHandler = (pagemove) => {
+    const currPage = this.state.currPage + pagemove;
+    this.setState({
+      currList : this.state.list[currPage],
+      currPage : currPage,
+      prevPage : !currPage,
+      nextPage : !this.state.list[currPage + 1]
+    })
+  }
+
   render(){
     return (
       <article>
         <h3>{this.getCategoryName()} 모음집의 다른 글귀</h3>
         <ul>
         {
-          this.state.list.map((obj,key)=>{
+          this.state.currList.map((obj,key)=>{
             const item = obj.node.frontmatter;
             return (
               <li key={key}>
@@ -53,6 +81,8 @@ class PostDetailList extends React.Component {
           })
         }
         </ul>
+        <button disabled={this.state.prevPage} onClick={()=>{this.onPagemoveHandler(-1)}}>왼쪽</button>
+        <button disabled={this.state.nextPage} onClick={()=>{this.onPagemoveHandler(1)}}>오른쪽</button>
       </article>
     )
   }
