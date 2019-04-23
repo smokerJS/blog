add15bfcc4133607c94ab3ea70eab2c7698ac6bf
Jung, jinsol <smokerjs.dev@gmail.com>
2019-04-23 18:46:24 +0900

    code highlighter setting

--- src/template/template.js
+++ src/template/template.js
@@ -3,31 +3,42 @@ import { graphql } from "gatsby";
 import SEO from "@components/seo";
 import PostDetailList from "@components/postDetailList";
 import { DiscussionEmbed } from "disqus-react";
+import Prism from "prismjs";
 import "@scss/github-markdown.css"
+import "@scss/prism.css"
 
-export default function Template({ data }) {
-  const { markdownRemark: post } = data;
-  const disqusShortname = "smokerjs";
-  const disqusConfig = {
-    identifier: `smokerjs.com/${post.frontmatter.path}`,
-    title: post.frontmatter.title,
-  };
-  return (
-    <React.Fragment>
-      <SEO title={post.frontmatter.title} keywords={[`gatsby`, `application`, `react`]} />
-      <div className="background-black"></div>
-      <section className="post-area">
-        <h1 className="post-title">{post.frontmatter.title}</h1>
-        <hr className="post-hr"/>
-        <b className="post-date">{post.frontmatter.date}</b>
-        <div className="post-content markdown-body" dangerouslySetInnerHTML={{ __html: post.html }} />
-        <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
-        <PostDetailList category={post.frontmatter.category} list={data.allMarkdownRemark.edges}/>
-      </section>
-    </React.Fragment>
-  );
+class Template extends React.Component {
+
+  componentDidMount() {
+    Prism.highlightAll();
+  }
+
+  render() {
+    const data = this.props.data;
+    const post = data.markdownRemark;
+    const disqusShortname = "smokerjs";
+    const disqusConfig = {
+      identifier: `smokerjs.com/${post.frontmatter.path}`,
+      title: post.frontmatter.title,
+    };
+    return (
+
+      <React.Fragment>
+        <SEO title={post.frontmatter.title} keywords={[`gatsby`, `application`, `react`]} />
+        <div className="background-black"></div>
+        <section className="post-area">
+          <h1 className="post-title">{post.frontmatter.title}</h1>
+          <hr className="post-hr"/>
+          <b className="post-date">{post.frontmatter.date}</b>
+          <div className="post-content markdown-body" dangerouslySetInnerHTML={{ __html: post.html }} />
+          <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
+          <PostDetailList category={post.frontmatter.category} list={data.allMarkdownRemark.edges}/>
+        </section>
+      </React.Fragment>
+    );
+  }
 }
-
+export default Template;
 export const pageQuery = graphql`
   query BlogPostByPath($path: String!) {
     allMarkdownRemark(limit: 1000, sort: { order: DESC, fields: [frontmatter___date] }) {
