20969b8bcd7fd6370430fe56008796c20e9fb981
Jung, jinsol <smokerjs.dev@gmail.com>
2019-04-29 17:10:42 +0900

    search class component => function component with react hooks

--- src/components/modal/search.js
+++ src/components/modal/search.js
@@ -2,34 +2,29 @@ import React from "react";
 import Modal from "@base/modal";
 import { connect } from "react-redux";
 import { toggleModalView } from "@state/app";
-class Search extends React.Component {
-  state = {
-    query : ''
+
+function Search ({isModalView, dispatch, search}) {
+  const [query, setQuery] = React.useState('');
+
+  const onChangeQueryHandler = (e) => {
+    setQuery(e.target.value);
   }
 
-  onChangeQueryHandler = (e) => {
-    this.setState({
-      query : e.target.value
-    })
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        {
-        this.props.isModalView &&
-          (
-            <Modal toggleFunction={this.props.toggleFunction}>
-              <input type="text" value={this.state.query} onChange={(e)=>{this.onChangeQueryHandler(e)}} placeholder={'어차피 찾는거 안나옴ㅋ'} />
-              <div>
-                <button onClick={()=>{this.props.dispatch(toggleModalView(!this.props.isModalView)); this.props.search(this.state.query);}}>검색</button>
-              </div>
-            </Modal>
-          )
-        }
-      </React.Fragment>
-    )
-  }
+  return (
+    <React.Fragment>
+      {
+      isModalView &&
+        (
+          <Modal>
+            <input type="text" value={query} onChange={(e)=>{onChangeQueryHandler(e)}} placeholder={'어차피 찾는거 안나옴ㅋ'} />
+            <div>
+              <button onClick={()=>{dispatch(toggleModalView(!isModalView)); search(query);}}>검색</button>
+            </div>
+          </Modal>
+        )
+      }
+    </React.Fragment>
+  )
 }
 
 export default connect(state => ({isModalView: state.app.isModalView}),null)(Search);
