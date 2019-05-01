8b3f6b27bd63476478292dc9afaf1c5c0236c7ee
Jung, jinsol <smokerjs.dev@gmail.com>
2019-05-01 17:41:21 +0900

    GNB component class => funtion with hooks

--- src/layout/globalNavigationBar.js
+++ src/layout/globalNavigationBar.js
@@ -1,109 +1,97 @@
 import { Link } from 'gatsby';
 import React from 'react';
 
-class GlobalNavigationBar extends React.Component {
-  state = {
-    open: false,
-  }
-
-  openHandler = () => {
-    this.setState({
-      open: !this.state.open,
-    });
-  }
-
-  menuClickHandler = () => {
-    this.setState({
-      open: false,
-    });
-  }
-
-  render() {
-    const { location } = this.props;
-    return (
-      <section
-        className={`nav-global-side-menu ${this.state.open ? 'open' : ''} ${
-          location.pathname === '/' ? 'home' : 'hide'
-        }`}
+const GlobalNavigationBar = ({ location }) => {
+  const [open, setOpen] = React.useState(false);
+  const openHandler = () => {
+    setOpen(!open);
+  };
+  const menuClickHandler = () => {
+    setOpen(!false);
+  };
+  return (
+    <section
+      className={`nav-global-side-menu ${open ? 'open' : ''} ${
+        location.pathname === '/' ? 'home' : 'hide'
+      }`}
+    >
+      <div
+        id="menuOpenBtn"
+        className={`${open ? '' : 'open'}`}
+        onClick={() => {
+          openHandler();
+        }}
       >
-        <div
-          id="menuOpenBtn"
-          className={`${this.state.open ? '' : 'open'}`}
-          onClick={() => {
-            this.openHandler();
-          }}
-        >
-          메뉴판
+        메뉴판
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <Link
+              to="/develop"
+              onClick={() => {
+                menuClickHandler();
+              }}
+            >
+              <strong
+                className={
+                  location.pathname.indexOf('develop') !== -1
+                    ? 'focus'
+                    : 'tab'
+                }
+              >
+                디베로먼투
+              </strong>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <strong>타로</strong>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <strong>라이푸</strong>
+            </Link>
+          </li>
+          <li>
+            <a href="https://github.com/smokerJS" target="_blank">
+              <strong>깃허부</strong>
+            </a>
+          </li>
+          <li>
+            <Link
+              to="/question"
+              onClick={() => {
+                menuClickHandler();
+              }}
+            >
+              <strong
+                className={
+                  location.pathname.indexOf('question') !== -1
+                    ? 'focus'
+                    : 'tab'
+                }
+              >
+                문의
+              </strong>
+            </Link>
+          </li>
+        </ul>
+        <div className="profile-group">
+          <img src={require('@images/common/img-profile.jpeg')} />
+          <dl>
+            <dt>직업</dt>
+            <dd>개발조무사</dd>
+            <dt>특기</dt>
+            <dd>고기태우기</dd>
+            <dt>목표</dt>
+            <dd>지구대폭발</dd>
+          </dl>
         </div>
-        <nav>
-          <ul>
-            <li>
-              <Link
-                to="/develop"
-                onClick={() => {
-                  this.menuClickHandler();
-                }}
-              >
-                <strong
-                  className={
-                    location.pathname.indexOf('develop') !== -1
-                      ? 'focus'
-                      : 'tab'
-                  }
-                >
-                  디베로먼투
-                </strong>
-              </Link>
-            </li>
-            <li>
-              <Link to="/">
-                <strong>타로</strong>
-              </Link>
-            </li>
-            <li>
-              <Link to="/">
-                <strong>라이푸</strong>
-              </Link>
-            </li>
-            <li>
-              <a href="https://github.com/smokerJS" target="_blank">
-                <strong>깃허부</strong>
-              </a>
-            </li>
-            <li>
-              <Link
-                to="/question"
-                onClick={() => {
-                  this.menuClickHandler();
-                }}
-              >
-                <strong
-                  className={
-                    location.pathname.indexOf('question') !== -1
-                      ? 'focus'
-                      : 'tab'
-                  }
-                >
-                  문의
-                </strong>
-              </Link>
-            </li>
-          </ul>
-          <div className="profile-group">
-            <img src={require('@images/common/img-profile.jpeg')} />
-            <dl>
-              <dt>직업</dt>
-              <dd>개발조무사</dd>
-              <dt>특기</dt>
-              <dd>고기태우기</dd>
-              <dt>목표</dt>
-              <dd>지구대폭발</dd>
-            </dl>
-          </div>
-        </nav>
-      </section>
-    );
-  }
-}
+      </nav>
+    </section>
+  );
+};
 
 export default GlobalNavigationBar;
