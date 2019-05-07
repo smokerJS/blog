a5ee33e76a5972112af568edb50de8df4c3e33d1
Jung, jinsol <smokerjs.dev@gmail.com>
2019-05-07 11:27:18 +0900

    eslint option update and code fix

--- src/base/tarotVideoParameter.js
+++ src/base/tarotVideoParameter.js
@@ -1,26 +1,7 @@
 import React from 'react';
 
 const TarotVideoParameter = ({ videoId, view }) => {
-  const [data, setData] = React.useState(null);
-
-  React.useEffect(() => {
-    if(!videoId) return;
-    const url = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${process.env.GATSBY_GOOGLE_YOUTUBE_API_KEY}&part=statistics,snippet`;
-    const xhr = new XMLHttpRequest();
-    xhr.onload = () => {
-      if (xhr.status === 200 || xhr.status === 201) {
-        const data = JSON.parse(xhr.responseText);
-        const uploadDate = new Date(data.items[0].snippet.publishedAt);
-        data.items[0].statistics.publishedAt = getFormatDate(uploadDate);
-        data.items[0].statistics.title = data.items[0].snippet.title;
-        data.items[0].statistics.description = data.items[0].snippet.description;
-        data.items[0].statistics.thumbnails = data.items[0].snippet.thumbnails.medium.url;
-        setData(data.items[0].statistics);
-      } else {}
-    };
-    xhr.open('GET', url);
-    xhr.send();
-  }, [videoId]);
+  const [param, setParam] = React.useState(null);
 
   const getFormatDate = (date) => {
     const year = date.getFullYear();
@@ -31,30 +12,50 @@ const TarotVideoParameter = ({ videoId, view }) => {
     return `${year}/${month}/${day}`;
   };
 
+  React.useEffect(() => {
+    if (!videoId) return;
+    const url = `https://www.googleapis.com/youtube/v3/videos?id=${videoId}&key=${process.env.GATSBY_GOOGLE_YOUTUBE_API_KEY}&part=statistics,snippet`;
+    const xhr = new XMLHttpRequest();
+    xhr.onload = () => {
+      if (xhr.status === 200 || xhr.status === 201) {
+        const data = JSON.parse(xhr.responseText);
+        const uploadDate = new Date(data.items[0].snippet.publishedAt);
+        data.items[0].statistics.publishedAt = getFormatDate(uploadDate);
+        data.items[0].statistics.title = data.items[0].snippet.title;
+        data.items[0].statistics.description = data.items[0].snippet.description;
+        data.items[0].statistics.thumbnails = data.items[0].snippet.thumbnails.medium.url;
+        setParam(data.items[0].statistics);
+      }
+    };
+    xhr.open('GET', url);
+    xhr.send();
+  }, [videoId]);
+
+
   return (
     <React.Fragment>
       {
-        data && (
+        param && (
           <div className="video-parameter">
             {
               !view && (
-                <img src={data.thumbnails} />
+                <img src={param.thumbnails} />
               )
             }
-            <h1>{data.title}</h1>
+            <h1>{param.title}</h1>
             {
               view && (
-                <p>{data.description}</p>
+                <p>{param.description}</p>
               )
             }
             <dl>
               <dt>조회수</dt>
-              <dd className="view-count">{data.viewCount}</dd>
+              <dd className="view-count">{param.viewCount}</dd>
               {
                 view && (
                   <React.Fragment>
                     <dt>좋아요</dt>
-                    <dd className="like-count">{data.likeCount}</dd>
+                    <dd className="like-count">{param.likeCount}</dd>
                   </React.Fragment>
                 )
               }
@@ -62,7 +63,7 @@ const TarotVideoParameter = ({ videoId, view }) => {
                 view && (
                   <React.Fragment>
                     <dt>덧글</dt>
-                    <dd className="comment-count">{data.commentCount}</dd>
+                    <dd className="comment-count">{param.commentCount}</dd>
                   </React.Fragment>
                 )
               }
@@ -70,7 +71,7 @@ const TarotVideoParameter = ({ videoId, view }) => {
                 view && (
                   <React.Fragment>
                     <dt>게시일</dt>
-                    <dd className="published-at">{data.publishedAt}</dd>
+                    <dd className="published-at">{param.publishedAt}</dd>
                   </React.Fragment>
                 )
               }
--- src/components/tarotContents.js
+++ src/components/tarotContents.js
@@ -7,10 +7,6 @@ const TarotContents = () => {
   const [nextPageToken, setNextPageToken] = React.useState('');
   const [prevPageToken, setPrevPageToken] = React.useState('');
 
-  React.useEffect(() => {
-    getYoutubeData();
-  }, []);
-
   const getYoutubeData = (pageToken = '') => {
     const optionParams = {
       part: 'id',
@@ -18,13 +14,13 @@ const TarotContents = () => {
       channelId: 'UCpO5KdEwqmS88dswUkYSgsw',
       order: 'date',
       maxResults: 6,
-      pageToken: pageToken,
+      pageToken,
     };
 
     let url = 'https://www.googleapis.com/youtube/v3/search?';
 
     for (const option in optionParams) {
-      url = `${url}${option}=${optionParams[option]}&`;
+      optionParams[option] && (url = `${url}${option}=${optionParams[option]}&`);
     }
     url = url.substr(0, url.length - 1);
 
@@ -35,11 +31,15 @@ const TarotContents = () => {
         setList(data.items);
         data.nextPageToken ? setNextPageToken(data.nextPageToken) : setNextPageToken('');
         data.prevPageToken ? setPrevPageToken(data.prevPageToken) : setPrevPageToken('');
-      } else {}
+      }
     };
     xhr.open('GET', url);
     xhr.send();
   };
+
+  React.useEffect(() => {
+    getYoutubeData();
+  }, []);
 
   return (
     <React.Fragment>
@@ -50,11 +50,11 @@ const TarotContents = () => {
         <div className="screen-area">
           <div className="video-box">
             <div>
-              <iframe src={`https://www.youtube.com/embed/rq6D0YtXHBQ`} frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen/>
+              <iframe src="https://www.youtube.com/embed/rq6D0YtXHBQ" title="video-screen" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
             </div>
           </div>
           <div className="content-box">
-            <TarotVideoParameter videoId={'rq6D0YtXHBQ'} view/>
+            <TarotVideoParameter videoId="rq6D0YtXHBQ" view />
           </div>
         </div>
         <hr className="post-hr" />
