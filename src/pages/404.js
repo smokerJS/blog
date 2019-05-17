add2df1cde58de13a78bec1cc2253901d49aa2fb
Jung, jinsol <smokerjs.dev@gmail.com>
2019-05-17 11:55:52 +0900

    404 page update

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,8 +4,7 @@ import SEO from '@components/seo';
 const NotFoundPage = () => (
   <React.Fragment>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>여기는 없는 페이지야 이 사람아!!!</h1>
   </React.Fragment>
 );
 
