6cac4463744895355c0df4e8498d211c96bac243
Jung, jinsol <smokerjs.dev@gmail.com>
2019-05-21 12:09:01 +0900

    transition component code remove

--- src/layout/transition.js
+++ src/layout/transition.js
@@ -1,5 +1,3 @@
-// Import necessary dependencies
-
 import React from 'react';
 import {
   TransitionGroup,
@@ -7,10 +5,8 @@ import {
 } from 'react-transition-group';
 import { connect } from 'react-redux';
 
-// This variable will be responsible for our animation duration
 const timeout = 500;
 
-// This object contains basic styles for animation, but you can extend them to whatever you like. Be creative!
 const getTransitionStyles = {
   entering: {
     position: 'absolute',
@@ -28,23 +24,18 @@ const getTransitionStyles = {
 
 class Transition extends React.PureComponent {
   render() {
-    // Destructuring props to avoid garbage this.props... in return statement
     const { children, location } = this.props;
 
     return (
-      // Using TransitionGroup and ReactTransition which are both
-      // coming from  'react-transition-group' and are required for transitions to work
       <TransitionGroup>
         <ReactTransition
-          // the key is necessary here because our ReactTransition needs to know when pages are entering/exiting the DOM
           key={location ? location.pathname : ''}
-          // duration of transition
           timeout={{
             enter: timeout,
             exit: timeout,
           }}
         >
-          {// Application of the styles depending on the status of page(entering, exiting, entered) in the DOM
+          {
             status => (
               <div
                 style={{
