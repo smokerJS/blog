18d2a6de429f905824344e97ac1da3592e9f93f8
Jung, jinsol <smokerjs.dev@gmail.com>
2019-07-18 15:05:40 +0900

    404 page content update

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ const NotFoundPage = () => (
   <React.Fragment>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <p>404 404 404 404</p>
   </React.Fragment>
 );
 
