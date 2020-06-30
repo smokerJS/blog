24b542c59cbca0092773ffe183ce36208881479c
Jung, Jinsol <smokerjs.dev@gmail.com>
2020-06-30 14:19:51 +0900

    layout component create

---
+++ layout/index.tsx
@@ -0,0 +1,32 @@
+import Header from './header';
+import Footer from './footer';
+import Head from 'next/head';
+
+export interface LayoutProps  { 
+    children: JSX.Element
+}
+
+export default function Layout({ children }: LayoutProps) {
+    return (
+        <>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+                <meta
+                name="description"
+                content="Learn how to build a personal website using Next.js"
+                />
+                {/* <meta
+                property="og:image"
+                content={`https://og-image.now.sh/${encodeURI(
+                    
+                )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+                /> */}
+                <meta name="og:title" content="후론투엔두" />
+                <meta name="twitter:card" content="summary_large_image" />
+            </Head>
+            <Header/>
+                <main>{children}</main>
+            <Footer/>
+        </>
+    )
+}
