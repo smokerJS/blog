a6cddbcdfdf90b903fbeca11cb5671ab5bd02064
Jung, Jinsol <smokerjs.dev@gmail.com>
2020-06-30 14:21:00 +0900

    app.js => app.tsx update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
+import { AppProps } from "next/app";
+
 import '../styles/global.css'
+import Layout from '../layout';
 
-type App = {
-  Component: any,
-  pageProps: {[key: string]: any}[]
-}
-
-export default function App({ Component, pageProps }: App) {
+const App = ({ Component, pageProps }: AppProps) => {
   return (
+    <Layout>
       <Component {...pageProps} />
+    </Layout>
   )
 }
+
+export default App;
