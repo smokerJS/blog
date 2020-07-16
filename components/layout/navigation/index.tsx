9d63613a90620b1b990ad22d057d491808a677e2
Jung, jin sol <smokerjs.dev@gmail.com>
2020-07-16 11:12:48 +0900

    navigation markup update

--- components/layout/navigation/index.tsx
+++ components/layout/navigation/index.tsx
@@ -3,9 +3,12 @@ import style from './style.module.scss';
 export default function Navigation() {
     return (
         <nav className={style.layout_navigation}>
-            <h1>
-                navi
-            </h1>
+            <button></button>
+            <section>
+                <ul>
+
+                </ul>
+            </section>
         </nav>
     )
 }
